b070ec676c415ab9cc8ec97e47e02d8fbe2e4c92
surendhargit <89027681+surendhargit@users.noreply.github.com>
2021-10-31 19:53:39 +0530

    Add files via upload

---
+++ Ex22.rb
@@ -0,0 +1,16 @@
+
+
+# "#" - the comment symbol. used to disable code
+# "=+-*/"  math operators.
+# "@"  instance variable.
+# "#{}" - passing in a variable
+# "==" - literally 'is equivalent to'
+# "%" - modulus. used to help format strings (and is the 'remainder' math operator).
+# "$" - global variable
+
+# puts("") - prints the contents to the output and then prints a new line.
+# .to_i - casts item to an integer
+# .to_f - casts item to a floating point
+# .to_s - casts item to a string
+# ARGV - argument variable. accepts and passes input from the user during script execution
+# "$stdin" - standard input. command line.
