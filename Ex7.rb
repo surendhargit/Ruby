cfad56d69da3cefd80e2fe1cca268edc0227e514
surendhargit <89027681+surendhargit@users.noreply.github.com>
2021-11-01 18:38:25 +0530

    Add files via upload

---
+++ Ex7.rb
@@ -0,0 +1,22 @@
+puts "Mary had a little lamb."
+puts "It's fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Watch that print vs. puts on this line. What's it do?  Answer: 'print' does not generate a newline
+print thing1 + thing2 + thing3 + thing4 + thing5 + thing6
+puts thing7 + thing8 + thing9 + thing10 + thing11 + thing12
+# Changing the above to a 'print' will generate the next prompt on the same line
