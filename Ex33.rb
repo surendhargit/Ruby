6fb86fea68f3b8572e2a6ab25954b31a586c3ebe
surendhargit <89027681+surendhargit@users.noreply.github.com>
2021-11-05 11:42:02 +0530

    Add files via upload

---
+++ Ex33.rb
@@ -0,0 +1,16 @@
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is (#{i})."
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is (#{i})."
+  puts "Again!"
+end
+
+puts "The final numbers: "
+
+numbers.each {|num| puts num }
